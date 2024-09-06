d8ee876f3ccae5ffe359bcacd5288f5a8a15b316
Natan Bronzatto <96526051+na7ank@users.noreply.github.com>
2024-09-06 00:14:42 +0000

    updates.

--- app.js
+++ app.js
@@ -1,86 +1,27 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
-const { Client } = require('./models/models'); // Importando o modelo Client
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
 
-// Configuração
-const configFilePath = path.join(__dirname, 'config.json');
-const configData = fs.readFileSync(configFilePath, 'utf8');
-const config = JSON.parse(configData);
+// Carregar variáveis de ambiente do arquivo .env
+dotenv.config();
 
-// Montando a URI de conexão ao MongoDB
-const mongoUri = `mongodb+srv://${config.mongoUser}:${config.mongoPassword}@${config.mongoHost}/${config.dbName}?retryWrites=true&w=majority`;
+// Conectar ao banco de dados
+connectDB();
 
-// Conectando ao MongoDB
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro na conexão com o MongoDB:'));
-db.once('open', () => {
-  console.log('Conectado ao MongoDB');
-});
-
+// Inicializar o app Express
 const app = express();
+
+// Middleware para analisar JSON
 app.use(express.json());
 
+// Incluir as rotas
+app.use('/clients', require('./routes/clientRoutes'));
+//app.use('/api/pets', require('./routes/petRoutes'));
+//app.use('/api/appointments', require('./routes/appointmentRoutes'));
+//app.use('/api/time_slots', require('./routes/timeSlotRoutes'));
 
-// Endpoint para listar todos os clientes
-app.get('/clients', async (req, res) => {
-    try {
-      const clients = await Client.find();
-      res.json(clients);
-    } catch (error) {
-      res.status(400).json({ message: 'Erro ao listar os clientes', error });
-    }
-  });
+// Porta do servidor
+const PORT = process.env.PORT || 5000;
 
-// Endpoint para criar um novo cliente
-app.post('/clients', async (req, res) => {
-  console.log(req.body)
-  try {
-    const newClient = new Client(req.body);
-    await newClient.save();
-    res.status(201).json(newClient);
-  } catch (error) {
-    res.status(400).json({ message: 'Erro ao criar o cliente', error });
-  }
-});
-
-// Endpoint para atualizar um cliente existente
-app.put('/clients/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const updatedClient = await Client.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
-    if (!updatedClient) {
-      return res.status(404).json({ message: 'Cliente não encontrado' });
-    }
-    res.json(updatedClient);
-  } catch (error) {
-    res.status(400).json({ message: 'Erro ao atualizar o cliente', error });
-  }
-});
-
-// Endpoint para deletar um cliente
-app.delete('/clients/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deletedClient = await Client.findByIdAndDelete(id);
-    if (!deletedClient) {
-      return res.status(404).json({ message: 'Cliente não encontrado' });
-    }
-    res.json({ message: 'Cliente deletado com sucesso' });
-  } catch (error) {
-    res.status(400).json({ message: 'Erro ao deletar o cliente', error });
-  }
-});
-
-
-// Iniciando o servidor
-const PORT = config.port || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// Iniciar o servidor
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
--- config/db.js
+++ config/db.js
@@ -1,19 +1,15 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-// Carregar variáveis de ambiente
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('MongoDB conectado!');
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB', error);
-    process.exit(1);
+    process.exit(1); // Encerra o processo em caso de erro de conexão
   }
 };
 
